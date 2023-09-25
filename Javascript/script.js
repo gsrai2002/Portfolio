8b9ece5ba0c93afc79ee349a601089cf3568f338
Ghanshyam Rai <gsrai082002@gmail.com>
2023-09-25 16:12:01 +0530

    Added scroll reveal and floating image

--- Javascript/script.js
+++ Javascript/script.js
@@ -38,3 +38,14 @@ window.addEventListener('scroll', () =>{
 });
 
 //Scroll reveal
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .portfolio-container, .skills-content, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-image', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
